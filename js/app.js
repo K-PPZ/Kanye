b8286d4e2f2603c54999009fc57cca295b17138b
korèn <kpapazian@edenschool.fr>
2022-04-22 16:30:14 +0200

    html and js

--- js/app.js
+++ js/app.js
@@ -2,25 +2,25 @@ fetch("https://api.kanye.rest/")
     .then((response) => response.json())
     .then((data) => {
 
-
+        let citations = data.quote;
         const Quote = document.getElementById("Quote");
-        
-        Quote.addEventListener('click', event => {
+
+            Quote.addEventListener('click', event => {
             
-            console.log(data.quote);
-            let Div = document.createElement("div");
-            Div.textContent += data.quote;
-            document.getElementById("main").appendChild(Div);
+                console.log(citations);
+                let Div = document.createElement("div");
+                Div.textContent = citations;
+                document.getElementById("main").appendChild(Div);
+    
+                let Button1 = document.createElement("button");
+                Button1.textContent = "Up";
+                document.getElementById("main").appendChild(Button1);
+    
+                let Button2 = document.createElement("button");
+                Button2.textContent = "Down";
+                document.getElementById("main").appendChild(Button2);
+              });
 
-            let Button1 = document.createElement("button");
-            Button1.textContent = "Up";
-            document.getElementById("main").appendChild(Button1);
-
-            let Button2 = document.createElement("button");
-            Button2.textContent = "Down";
-            document.getElementById("main").appendChild(Button2);
-          });
-
-
+        
     })
     .catch((error) => console.log(error));
